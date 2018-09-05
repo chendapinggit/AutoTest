f302419894fdcf2792dd5bec7ebb73ee203d0f9b
chendapinggit <chendapinggit>
2018-09-05 11:10:26 +0800

    接口测试1

---
+++ Api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import requests,json,unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_upper(self):
+        """
+        测试是否大小写正常
+        ：:return;
+        """
+        self.assertTrue('F00'.isupper())
+        self.assertFalse('foo'.isupper())
+    def test_split(self):
+         """
+         测试hello world
+         :return:
+         """
+         s='hello world'
+         self.asserEqual(s.split(),['hello','world'])
+         with self.assertRaises(TypeError):
+             s.split(2)
+if __name__=='__main__':
+    unittest.main()
